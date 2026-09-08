c3866468da410b96792a4ce29c0c072ceb459ccf
Wilson, Carter <carter.j.wilson@okstate.edu>
2026-09-08 12:27:05 -0500

    Add project files.

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CW_Basic_App_GitHub.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
